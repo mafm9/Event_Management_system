1e78294bff0c9d2ff4a7d00129f7336f17bb07d7
mafm9 <miguelafm2@gmail.com>
2021-05-23 02:23:30 -0500

    Added docstrings for functions

--- event.py
+++ event.py
@@ -1,4 +1,3 @@
-import json
 import datetime as dt
 import os
 import random
@@ -8,32 +7,42 @@ cnx = mysql.connector.connect(user=os.getenv('db_user'), database='events')
 
 
 class event:
+    """
+    a class that represents a celebratory event
+    """
     def __init__(self, name, date_of_event, event_type) -> None:
+        """
+        :param name: name of the person who the event is celebrating
+        :type name: str
+        :param date_of_event: the date the event will take place
+        :type date_of_event: str
+        :param event_type: the type of event the person will be celebrating
+        :type event_type: str
+        """
         self.name = name
         self.date_of_event = dt.datetime.strptime(date_of_event, "%m-%d-%Y").strftime('%Y-%m-%d')
         self.event_type = event_type
 
     def __str__(self) -> str:
+        """
+        print out instance variables in a readable format
+
+        :return: formatted string
+        :rtype: str
+        """
         return f"date_of_event: {self.date_of_event}\n" \
                f"name: {self.name}\n" \
                f"event: {self.event_type}\n" \
                f"gift idea: {gift(self.event_type)}\n"
 
 
-def new_event_dict() -> dict:
-    date_of_event = input("please enter date of event: ")
-    while not validate_date(date_of_event):
-        date_of_event = input("Date format incorrect please input date as MM-DD-YYYY: ")
-    date_of_event = dt.datetime.strptime(date_of_event, "%m-%d-%Y").strftime('%Y-%m-%d')
-    name = input("Please enter who's event it is: ").title()
-    event_type = input("Please enter what the event is: ").capitalize()
-    return {"date_of_event": date_of_event,
-            "name": name,
-            "type_of_event": event_type
-            }
-
-
 def new_event_obj():
+    """
+    returns an event instance from user input and converts date from M-d-Y to Y-M-d
+
+    :return: instance of event class
+    :rtype: event
+    """
     date_of_event = input("please enter date of event: ")
     while not validate_date(date_of_event):
         date_of_event = input("Date format incorrect please input date as MM-DD-YYYY: ")
@@ -43,6 +52,14 @@ def new_event_obj():
 
 
 def validate_date(event_date) -> bool:
+    """
+    checks date to make sure event is in month-day-year format
+
+    :param event_date: string of date of event in month-day-year format
+    :type event_date: str
+    :return: true if date is in correct format false if it is not
+    :rtype: bool
+    """
     try:
         dt.datetime.strptime(event_date, "%m-%d-%Y")
         return True
@@ -51,10 +68,21 @@ def validate_date(event_date) -> bool:
 
 
 def event_date_formatting(event_date) -> str:
+    """
+    converts date from month-day-year to year-month-day
+
+    :param event_date: string of date of event in month-day-year format
+    :type event_date: str
+    :return: converted date
+    :rtype: str
+    """
     return dt.datetime.strptime(event_date, "%m-%d-%Y").strftime('%Y-%m-%d')
 
 
 def add():
+    """
+    add a new event into the database
+    """
     cursor = cnx.cursor(buffered=True)
     new_entry = new_event_obj()
     insert = ("INSERT INTO event(name,eventDate,eventType)"
@@ -64,6 +92,11 @@ def add():
 
 
 def edit():
+    """
+    edit existing values by asking user to input information for the event they want to change
+    and what part of the event they want to change
+
+    """
     cursor_select = cnx.cursor(buffered=True)
     cursor_update = cnx.cursor(buffered=True)
     select = ("SELECT eventid From event "
@@ -104,6 +137,9 @@ def edit():
 
 
 def delete():
+    """
+    delete entries in database
+    """
     delete_event = new_event_obj()
     cursor_select = cnx.cursor(buffered=True)
     cursor_delete = cnx.cursor(buffered=True)
@@ -119,6 +155,9 @@ def delete():
 
 
 def name_search():
+    """
+    asks user for a persons name and search the database to find entries with that name and display them
+    """
     name = input("Please enter name you're looking for: ").title()
     cursor = cnx.cursor(buffered=True)
     select = ("SELECT * from event "
@@ -131,6 +170,9 @@ def name_search():
 
 
 def event_search():
+    """
+    ask user for a type of event and searches database and displays events that are of that type
+    """
     event_type = input("Please enter event you're looking for: ").capitalize()
     cursor = cnx.cursor(buffered=True)
     select = ("SELECT * from event "
@@ -143,6 +185,9 @@ def event_search():
 
 
 def upcoming():
+    """
+    print events that are upcoming within the next 7 days
+    """
     today = dt.datetime.now().date()
     events = []
     cursor = cnx.cursor(buffered=True)
@@ -162,6 +207,9 @@ def upcoming():
 
 
 def notification():
+    """
+    print events that are coming up within the next two days
+    """
     today = dt.datetime.now().date()
     events = []
     cursor = cnx.cursor(buffered=True)
@@ -181,10 +229,19 @@ def notification():
 
 
 def gift(type_of_event):
+    """
+    looks for gifts based on the type of event and return a random gift idea and present it to the user
+
+    :param type_of_event: the type of the event that you are celebrating
+    :type type_of_event: str
+    :return: the name of a gift that is appropriate for the type of event that
+    :rtype:str
+    """
     cursor = cnx.cursor(buffered=True)
     select_events = "SELECT DISTINCT eventType from gifts"
     cursor.execute(select_events)
     events = cursor.fetchall()
+    events= [item[0] for item in events]
     if type_of_event not in events:
         type_of_event = "Misc"
     select_gifts = ("SELECT giftIdea FROM gifts "
